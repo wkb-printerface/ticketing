0f84a2277e28d74d4ea842b13ceba4e3f6e79b83
wkb-printerface <wierish.bansie@printerface.io>
2022-08-01 09:19:02 +0200

    klaar2

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { app } from "./app";
 
 const start = async () => {
 
-  console.log('Starting ....')
+  console.log('Starting .....')
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
