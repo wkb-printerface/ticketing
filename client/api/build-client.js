e1abebe747dc94628b0393f4333ae142f7cb2fe9
wkb-printerface <wierish.bansie@printerface.io>
2022-08-01 12:54:23 +0200

    update hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ const buildClient = ({ req }) => {
     // we are on the server
 
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.ticketing-wiers-prod-app.xyz",
       headers: req.headers,
     });
   } else {
