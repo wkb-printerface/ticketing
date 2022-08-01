857e2bf3c228af8e6753dd81d34e5974899aab05
wkb-printerface <wierish.bansie@printerface.io>
2022-08-01 09:26:16 +0200

    klaar3

--- client/pages/index.js
+++ client/pages/index.js
@@ -17,7 +17,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 
   return (
     <div>
-      <h2>Tickets</h2>
+      <h2>Tickets!!</h2>
       <table className="table">
         <thead>
           <tr>
